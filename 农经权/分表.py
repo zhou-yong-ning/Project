31c15f74c7298a107aac2f6eeeab2ef01d9730a2
zhou-yong-ning <102033184+zhou-yong-ning@users.noreply.github.com>
2022-07-17 15:06:16 +0800

    Update 分表.py

--- 农经权/分表.py
+++ 农经权/分表.py
@@ -3,6 +3,9 @@ import os
 # 自动调整列宽
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
+
+
+# 自动调整列宽方法
 def reset_col(filename):
     wb = load_workbook(filename)
     for sheet in wb.sheetnames:
@@ -13,26 +16,30 @@ def reset_col(filename):
             index = list(df.columns).index(col)  # 列序号
             letter = get_column_letter(index + 1)  # 列字母
             collen = df[col].apply(lambda x: len(str(x).encode())).max()  # 获取这一列长度的最大值 当然也可以用min获取最小值 mean获取平均值
-            ws.column_dimensions[letter].width = collen * 1.2 + 2  # 也就是列宽为最大长度*1.2 可以自己调整
+            ws.column_dimensions[letter].width = collen * 1.2 + 2  # 也就是列宽为最大长度*1.2
     wb.save(filename)
-col = input('请输入列标题: ')
-Current_Folder_path = os.getcwd()
-# 遍历当前文件夹下的文件
-file_list = os.listdir(Current_Folder_path)
-if not os.path.exists(Current_Folder_path + '\\New Folder'):
-    os.mkdir(Current_Folder_path + '\\New Folder')
-docx_list = [a for a in file_list if a.endswith('.xlsx')]
-print('正在读取文件')
-df = pd.read_excel(Current_Folder_path + '\\' + docx_list[0], dtype='object')
-print('文件读取成功')
-# 获取某一列唯一值
-listType = df[col].unique()
-# 循环筛选导出
-for i in listType:
-    df0 = df.loc[(df[col] == i)]
-    df0.to_excel(Current_Folder_path + '\\New Folder\\' + str(i) + '.xlsx', index=False)
-    print(str(i) + '导出成功')
-file_list = os.listdir(Current_Folder_path + '\\New Folder')
-for j in file_list:
-    reset_col(Current_Folder_path + '\\New Folder\\' + str(j))
-    print(str(j) + '格式转换成功')
+
+if __name__ == '__main__':
+    # col = input('请输入列标题: ')
+    col = 'FBFBM'
+    Current_Folder_path = os.getcwd()
+    # 遍历当前文件夹下的文件
+    file_list0 = os.listdir(Current_Folder_path)
+    if not os.path.exists(Current_Folder_path + '\\NewFolder'):
+        os.mkdir(os.path.join(Current_Folder_path, 'NewFolder'))
+    file_list1 = [a for a in file_list0 if a.endswith('.xlsx')]
+    print('正在读取文件')
+    df = pd.read_excel(os.path.join(Current_Folder_path, file_list1[0]), dtype='object')
+    df1 = pd.read_excel(os.path.join(Current_Folder_path, 'FBF.xlsx'), index_col='FBFBM', dtype='object')
+    print('文件读取成功')
+    # 获取某一列唯一值
+    listType = df[col].unique()
+    # 循环筛选导出
+    for i in listType:
+        df0 = df.loc[(df[col] == i)]
+        # 这里需要转int类型，不然会报错，真不知道为什么，服了
+        i = df1.loc[int(i), 'FBFMC']
+        df0.to_excel(os.path.join(Current_Folder_path, 'NewFolder', i + '.xlsx'), index=False)
+        # 自动调整列宽
+        # reset_col(os.path.join(Current_Folder_path, 'NewFolder', str(i) + '.xlsx'))
+        print(i + ' 表格导出成功')
