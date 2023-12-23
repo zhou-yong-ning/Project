b095cf6606b0a5051840f9d390db8f3789f287b0
zhou-yong-ning <102033184+zhou-yong-ning@users.noreply.github.com>
2023-12-23 11:23:19 +0800

    Add files via upload

--- 增减挂钩备案/多层路径重命名(表格对应).py
+++ 增减挂钩备案/多层路径重命名(表格对应).py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-rename_df = pd.read_excel('rename.xlsx', dtype='object', index_col='old')
+rename_df = pd.read_excel('rename.xlsx', dtype='object')
 # 获取当前文件夹路径
 Current_Folder_path = os.getcwd()
 # 建立旧文件名路径列表
@@ -12,9 +12,12 @@ for Folder_path, folder_path, file_path in os.walk(Current_Folder_path):
         rename_list.append(Folder_path)
     rename_list = list(set(rename_list))
 for old in rename_list:
-    new_1 = str(old.split('\\')[-1])
-    new_2 = '\\'.join(old.split('\\')[0:-1])
-    new_3 = rename_df.loc[new_1, 'new']
-    new = os.path.join(new_2, new_3)
-    print(new)
-    os.rename(old, new)
+    # 旧文件夹名
+    old_name = str(old.split('\\')[-1])
+    # 子文件夹所在路径
+    path_floder = '\\'.join(old.split('\\')[0:-1])
+    # 新文件夹名
+    new_name = rename_df.loc[rename_df['old'] == old_name, 'new'].tolist()[0]
+    # 新文件夹路径
+    new_name_path = os.path.join(path_floder, new_name)
+    os.rename(old,new_name_path)
